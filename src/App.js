2bbce28a83bd65c17dfad7c9bd93cd57653b6b30
avatarfreak <avatarfreak>
2019-07-23 02:25:15 +0530

    added increment and decrement func

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import Footer from "./components/Footer";
 import Timer from "./components/Timer";
 import Break from "./components/Break";
 import Session from "./components/Session";
-import { throwStatement } from "@babel/types";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sessionTime: 5,
-      breakTime: 4,
-      time: 3,
+      sessionTime: 25,
+      breakTime: 5,
+      time: 25 * 60,
       active: false,
       status: "session"
     };
 
     this.initialize = this.initialize.bind(this);
     this.startTimer = this.startTimer.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
     this.countDown = this.countDown.bind(this);
   }
   //Reset btn
@@ -56,19 +57,49 @@ class App extends Component {
   }
   componentDidUpdate(prevProps) {
     if (this.state.time === -1 && this.state.status === "session") {
-      this.setState({ time: this.state.breakTime, status: "break" });
+      this.setState({ time: this.state.breakTime * 60, status: "break" });
       this.audio.play();
     }
     if (this.state.time === -1 && this.state.status === "break") {
       this.setState({
-        time: this.state.sessionTime,
+        time: this.state.sessionTime * 60,
         status: "session"
       });
       this.audio.play();
     }
   }
 
-  increment() {}
+  increment(e) {
+    let { breakTime, sessionTime } = this.state;
+    if (e.target.id === "break-increment") {
+      this.setState({
+        breakTime: breakTime < 59 ? breakTime + 60 : breakTime * 60
+      });
+    }
+
+    if (e.target.id === "session-increment") {
+      this.setState({
+        sessionTime: sessionTime < 59 ? sessionTime + 1 : sessionTime,
+        time: sessionTime * 60
+      });
+    }
+  }
+
+  decrement(e) {
+    let { breakTime, sessionTime } = this.state;
+    if (e.target.id === "break-decrement") {
+      this.setState({
+        breakTime: breakTime > 1 ? breakTime - 1 : breakTime
+      });
+    }
+
+    if (e.target.id === "session-decrement") {
+      this.setState({
+        sessionTime: sessionTime > 1 ? sessionTime - 1 : sessionTime,
+        time: sessionTime * 60
+      });
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -79,8 +110,17 @@ class App extends Component {
           startTimer={this.startTimer}
           active={this.state.active}
         />
-        <Break display={this.state.breakTime} />
-        <Session display={this.state.sessionTime} />
+        <Break
+          display={this.state.breakTime}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
+        <Session
+          display={this.state.sessionTime}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
+
         <audio
           id="beep"
           src="https://s3-us-west-1.amazonaws.com/benjaminadk/Data+synth+beep+high+and+sweet.mp3"
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,14 +3,14 @@ import Time from "./Time";
 
 export class Timer extends Component {
   render() {
-    let { intialize, display, status, startTimer, active } = this.props;
+    let { initialize, display, status, startTimer, active } = this.props;
     return (
       <div className="time-box">
-        <div id="timer-label">{(status) ? 'Break': 'Session'}</div>
+        <div id="timer-label">{"session" === status ? "session" : "Break"}</div>
         <div id="time-left">
           <Time time={display} />
         </div>
-        <button id="reset" onClick={intialize}>
+        <button id="reset" onClick={initialize}>
           Reset
         </button>
         <button id="start_stop" onClick={startTimer}>
