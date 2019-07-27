94927ed98dfc9a604f7657e3dc8e83626d35a2cf
avatarfreak <avatarfreak>
2019-07-27 20:03:53 +0530

    refactor code

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,18 +1,57 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
+//import PropTypes from "prop-types";
 
-function Time(props) {
-  const minutes = Math.floor(props.time / 60);
-  const seconds = Math.floor(props.time % 60);
-  let timeDisplay = `${minutes < 10 ? "0" + minutes : minutes}:${
-    seconds < 10 ? "0" : ""
-  }${seconds}`;
+export class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialOffset: 679
+    };
+  }
 
-  return <div id="time-left">{timeDisplay}</div>;
+  componentDidUpdate() {
+    let { sessionTime, breakTime, timeRemaining, status } = this.props;
+    let { initialOffset } = this.state;
+    let circum;
+    if (!status && timeRemaining > -1) {
+      console.log(sessionTime);
+      sessionTime = sessionTime * 60;
+      circum = (initialOffset / sessionTime) * timeRemaining;
+      console.log(`WorkTime: ${sessionTime}`);
+      console.log(`circum: ${circum}`);
+      console.log(`timeRemaining: ${timeRemaining}`);
+    } else {
+      breakTime = breakTime * 60;
+      circum = (initialOffset / breakTime) * timeRemaining;
+      console.log(`BreaTimek: ${breakTime}`);
+    }
+
+    document
+      .getElementById("circle_animation")
+      .setAttributeNS(null, "stroke-dashoffset", circum);
+  }
+
+  displayTime() {
+    let { timeRemaining } = this.props;
+    let minutes = Math.floor(timeRemaining / 60);
+    let seconds = Math.floor(timeRemaining % 60);
+
+    let timeDisplay = `${minutes < 10 ? "0" + minutes : minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`;
+    return timeDisplay;
+  }
+  render() {
+    return (
+      <div className="pie" id="pie">
+        <div className="block">
+          <svg>
+            <circle id="circle_animation" r="108" cx="110" cy="110" />
+          </svg>
+          <span id="time-left">{this.displayTime()}</span>
+        </div>
+      </div>
+    );
+  }
 }
-
-Time.propTypes = {
-  time: PropTypes.number.isRequired
-};
-
 export default Time;
