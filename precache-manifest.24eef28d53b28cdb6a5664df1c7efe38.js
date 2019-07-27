71e68b6e49d89021e4455fdc542be8d4b211f616
avatarfreak <avatarfreak>
2019-07-27 22:32:16 +0530

    Updates

---
+++ precache-manifest.24eef28d53b28cdb6a5664df1c7efe38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97828d0478cb5224f1ca7f25e2a834c",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "ea07cb61ba93b01a7c56",
+    "url": "/react-pomodoro/static/css/main.9db3159f.chunk.css"
+  },
+  {
+    "revision": "35b56a3c744b020dce95",
+    "url": "/react-pomodoro/static/js/2.59429c0b.chunk.js"
+  },
+  {
+    "revision": "ea07cb61ba93b01a7c56",
+    "url": "/react-pomodoro/static/js/main.361c6954.chunk.js"
+  },
+  {
+    "revision": "6d7c46671e7bd68077c9",
+    "url": "/react-pomodoro/static/js/runtime~main.69fb4663.js"
+  }
+]);
